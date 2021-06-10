d6be8a1a96440f47b9f787d5d2fb7c1eb3130b67
Andris Tsildermanis <yanich.2213@gmail.com>
2021-06-10 21:19:56 +0400

    clear and config project

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-plusplus': 0,
+    'no-debugger': 'off',
+    'vue/attributes-order': 1,
+    'vue/no-confusing-v-for-v-if': 2,
+    "linebreak-style": 0,
+    "no-restricted-syntax": 0,
+    "import/no-cycle": 0,
+    'no-param-reassign': ["error", { "props": false }]
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/airbnb'
+  ]
+};
