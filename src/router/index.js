d11fdb930076b876d34f0a5f9d39f9d43a7f2b6e
Andris Tsildermanis <yanich.2213@gmail.com>
2021-06-13 17:51:03 +0400

    config routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,41 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import store from '../store';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
+      name: 'home',
+      component: () => import('../pages/PageHome.vue'),
+      meta: { noAuth: false },
+    },
+    {
+      path: '/login',
       name: 'login',
       component: () => import('../pages/PageLogin.vue'),
       meta: { noAuth: true },
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const isAuth = store.getters['auth/isAuthenticated'];
+
+  if (to.matched.every((record) => record.meta.noAuth)) {
+    if (to.name === 'login' && isAuth) {
+      next({ name: 'home' });
+    } else {
+      next();
+    }
+  } else if (isAuth) {
+    next();
+  } else {
+    next({ name: 'login' });
+  }
+});
+
+export default router;
