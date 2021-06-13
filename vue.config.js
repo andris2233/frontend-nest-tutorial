a856cd5e588a930c9bdeafa96247c64188351d08
Andris Tsildermanis <yanich.2213@gmail.com>
2021-06-13 15:11:13 +0400

    added vue.config file

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `
+        @import "@/assets/scss/variables.scss";
+        `,
+      },
+    },
+  },
+};
