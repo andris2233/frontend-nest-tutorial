f9c88fa57fc628ea7f29bc5d706124b6791144f4
Andris Tsildermanis <yanich.2213@gmail.com>
2021-06-13 17:50:53 +0400

    add pages

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="home">
+    <button @click="onLogout">
+      logout
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PageHome',
+  methods: {
+    onLogout() {
+      this.$store.dispatch('auth/logout');
+      this.$router.push({ name: 'login' });
+    },
+  },
+};
+</script>
--- src/pages/PageLogin.vue
+++ src/pages/PageLogin.vue
@@ -1,17 +1,32 @@
 <template>
   <LayoutLogin>
-    <section></section>
+    <TheLoginForm
+      @login="onLogin"
+    />
   </LayoutLogin>
 </template>
 
 <script>
-import LayoutLogin from '../layout/LayoutLogin.vue';
+import LayoutLogin from '@/layout/LayoutLogin.vue';
+import TheLoginForm from '@/components/login/TheLoginForm.vue';
 
 export default {
   name: 'PageLogin',
-  components: { LayoutLogin },
+  components: {
+    LayoutLogin,
+    TheLoginForm,
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async onLogin(form) {
+      await this.$store.dispatch('auth/login', form);
+      this.$router.push({ name: 'home' });
+    },
+  },
 };
 </script>
-
-<style lang="scss" scoped>
-</style>
